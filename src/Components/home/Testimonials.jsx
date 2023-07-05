f45aab1e1162502a5c40bfdf68c9f6d14f665d49
shashankk748 <shashank.smart748@gmail.com>
2023-07-05 16:00:30 +0530

    Testimonials Page tweaks

--- src/Components/home/Testimonials.jsx
+++ src/Components/home/Testimonials.jsx
@@ -1,22 +1,23 @@
 import React from "react";
+import { UilComment } from "@iconscout/react-unicons";
 
 export default function Testimonials() {
   return (
     <section className="bg-[rgb(248,248,248)] mt-[10vh]">
       <div className="flex justify-center">
         <div className=" flex flex-col justify-center w-2/3 ">
-          <div className="mt-[10vh] flex flex-col text-center gap-5">
-            <div className=" text-xl font-bold">Reviewed by People</div>
+          <div className="mt-[10vh] flex flex-col text-center gap-8">
+            <div className=" text-2xl font-semibold">Reviewed by People</div>
             <div className=" text-5xl font-extrabold">
               Client's Testimonials
             </div>
-            <div className="  text-desc">
+            <div className=" translate-x-[50%] w-3/6 text-desc">
               Discover the positive impact we've made on the our clients by
               reading through their testimonials. Our clients have experienced
               our service and results, and they're eager to share their positive
               experiences with you.
             </div>
-            <div className=" flex justify-around w-full gap-6 mt-[12vh]">
+            <div className=" flex justify-around w-full gap-6 mt-[5vh]">
               <div className=" bg-white text-black sahdow-black shadow-2xl p-9 w-1/2">
                 <p className=" text-2xl text-left ">
                   "We rented a car from this website and had an amazing
@@ -32,6 +33,7 @@ export default function Testimonials() {
                   <p className=" flex translate-x-[20%] translate-y-[30%] font-bold">
                     Parry Hotter
                   </p>
+                  <UilComment />
                 </div>
               </div>
               <div className=" bg-white text-black sahdow-black shadow-2xl p-9 w-1/2">
@@ -45,7 +47,10 @@ export default function Testimonials() {
                     alt=""
                     className=" h-[8vh] aspect-auto rounded-full"
                   />
-                  Ron Rizzly
+                  <p className=" flex translate-x-[20%] translate-y-[30%] font-bold">
+                    Ron Rizzly
+                  </p>
+                  <UilComment />
                 </div>
               </div>
             </div>
