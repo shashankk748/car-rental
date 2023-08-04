5ea74a32f1a3c7656a844a9e5f4daadc532c2a7d
shashankk748 <shashank.m748@gmail.com>
2023-08-04 09:26:02 +0530

    normal tweaks

--- src/Components/home/Faq.jsx
+++ src/Components/home/Faq.jsx
@@ -1,7 +1,11 @@
 import React from "react";
-<section>
-  <div>
-    {/* <h1> FAQ</h1> */}
-    <h1 className=" text-3xl font-bold">Frequently Asked Questions</h1>
-  </div>
-</section>;
+export default function Faq() {
+  return (
+    <section>
+      <div>
+        {/* <h1> FAQ</h1> */}
+        <h1 className=" text-3xl font-bold">Frequently Asked Questions</h1>
+      </div>
+    </section>
+  );
+}
