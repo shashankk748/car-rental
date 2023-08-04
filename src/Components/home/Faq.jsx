4218d024ee1bc53bee7dcf39a81542186900bd8c
shashankk748 <shashank.m748@gmail.com>
2023-08-04 09:51:54 +0530

    normal tweaks

--- src/Components/home/Faq.jsx
+++ src/Components/home/Faq.jsx
@@ -2,9 +2,20 @@ import React from "react";
 export default function Faq() {
   return (
     <section>
-      <div>
-        {/* <h1> FAQ</h1> */}
-        <h1 className=" text-3xl font-bold">Frequently Asked Questions</h1>
+      <div className=" mt-[10vh] flex justify-center">
+        <div className=" flex flex-col justify-center gap-6">
+          {/* <h1> FAQ</h1> */}
+          <div className=" flex flex-col text-center w-full gap-4">
+            <h1 className=" text-6xl font-bold">Frequently Asked Questions</h1>
+            <p className=" text-desc text-center w-2/3 translate-x-[25%]">
+              Frequently Asked Questions About the Car Rental Booking Process on
+              Our Website: Answers to Common Concerns and Inquiries.
+            </p>
+          </div>
+          <div></div>
+
+          <div></div>
+        </div>
       </div>
     </section>
   );
